c47d571c787e45a77bd6a3571cc0314b7be79385
GGen10 <master.e3@gmail.com>
2025-03-05 16:05:57 +0100

    redefined functions to toggle pins on maps

--- resources/views/map/partials/locations-list.blade.php
+++ resources/views/map/partials/locations-list.blade.php
@@ -1,21 +1,37 @@
-<div class="m-4 max-h-[350px] overflow-y-scroll sm:rounded-lg">
-  <button class="btn btn-gray-200 rounded px-2 font-bold dark:bg-gray-700 dark:text-gray-100" onclick="showAllOnMap()">
+<div class="scroll m-4 max-h-[350px] overflow-y-scroll sm:rounded-lg">
+  <button id="show-all-on-map-button" class="btn btn-gray-200 rounded px-2 font-bold dark:bg-gray-700 dark:text-gray-100"
+    onclick="toggleShowAllOnMap()">
     Show all on map
   </button>
+  <script>
+    function toggleShowAllOnMap() {
+      const button = document.getElementById('show-all-on-map-button');
+      if (button.innerText === 'Show all on map') {
+        button.innerText = 'Hide all on map';
+        button.classList.add('bg-red-500');
+        showAllOnMap();
+      } else {
+        button.innerText = 'Show all on map';
+        button.classList.remove('bg-red-500');
+        hideAllOnMap();
+      }
+    }
+  </script>
+
   @foreach ($lines as $line)
     <div class="m-4 flex justify-between overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
       <div class="h-100 flex flex-col justify-between gap-1 p-3 dark:text-gray-100">
-        <span>{{ $line['Title'] }}</span>
-        <a href="{{ $line['url'] }}" target="_blank">{{ Str::limit($line['url'], 25, '...') }}</a>
+        <span>{{ $line->Title }}</span>
+        <a href="{{ $line->url }}" target="_blank"> link </a>
       </div>
       <div class="h-100 flex flex-col justify-between gap-1 p-3 dark:text-gray-100">
         @if ($line->coord)
           <button class="btn btn-gray-200 rounded px-2 font-bold dark:bg-gray-700 dark:text-gray-100"
-            onclick="showOnMap({{ $line->coord->latitude }}, {{ $line->coord->longitude }}, '{{ hash('sha256', $line['url']) }}')">
+            onclick="toggleOnMap({{ $line->coord->latitude }}, {{ $line->coord->longitude }}, '{{ $line->coord->id }}')">
             Show on map
           </button>
           <button class="btn btn-gray-200 rounded px-2 font-bold dark:bg-gray-700 dark:text-gray-100"
-            onclick="centerOnMap({{ $line->coord->latitude }}, {{ $line->coord->longitude }}, '{{ hash('sha256', $line['url']) }}')">
+            onclick="centerOnMap({{ $line->coord->latitude }}, {{ $line->coord->longitude }}, '{{ $line->coord->id }}')">
             Center
           </button>
         @endif
@@ -29,36 +45,61 @@
 <script>
   var icons = {};
 
-  function showOnMap(latitude, longitude, id = "") {
-    if (icons[id] == undefined) {
-      var icon = L.marker([latitude, longitude], {
-        icon: coffeeCupIcon
-      }).addTo(map);
-      icons[id] = icon;
-      console.log("show: " + id);
-    } else {
-      map.removeLayer(icons[id]);
-      icons[id] = undefined;
-    }
-    console.log("icons: " + icons[id]);
+  function hideOnMap(id) {
+    map.removeLayer(icons[id]);
+    delete icons[id];
+    console.log("hiding: " + id);
+
   }
 
-  function centerOnMap(latitude, longitude, id = "") {
-    map.setView([latitude, longitude], 15);
+  function showOnMap(latitude, longitude, id) {
+    var icon = L.marker([latitude, longitude], {
+      icon: coffeeCupIcon
+    }).addTo(map);
+    icons[id] = icon;
+    console.log("showing: " + id);
+  }
+
+  function toggleOnMap(latitude, longitude, id) {
+    if (icons[id]) {
+      hideOnMap(id);
+    } else {
+      showOnMap(latitude, longitude, id);
+    }
+  }
+
+  function centerOnMap(latitude, longitude, id) {
+    map.setView([latitude, longitude], 7, {
+      animate: true,
+      duration: 2.5
+    });
     console.log("center: " + id);
   }
 
   function showAllOnMap() {
-    fetch('/map/fetchAllCoordinates')
-      .then(response => response.json())
-      .then(data => {
-        data.forEach((line) => {
-          if (icons[line.id] != undefined) return;
-          showOnMap(line.latitude, line.longitude, line.id);
+    axios.get("{{ route('map.fetchAllCoordinates') }}")
+      .then(response => {
+        console.log(response.data);
+        response.data.forEach((coordinate) => {
+          if (!icons[coordinate.id])
+            showOnMap(coordinate.latitude, coordinate.longitude, coordinate.id);
         });
 
-        console.log("show all");
-        console.log(all);
+        console.log(icons);
+        console.log("showing all");
+      });
+  }
+
+  function hideAllOnMap() {
+    axios.get("{{ route('map.fetchAllCoordinates') }}")
+      .then(response => {
+        console.log(response.data);
+        response.data.forEach((coordinate) => {
+          if (icons[coordinate.id])
+            hideOnMap(coordinate.id);
+        });
+
+        console.log("hiding all");
       });
   }
 </script>
