c2dfdaf748dd50f3c5b6e774a329b6e33759b8f3
GGen10 <master.e3@gmail.com>
2025-03-06 19:10:28 +0100

    implemented edgebuffer for map and edited csv file

--- resources/views/map/partials/view-map.blade.php
+++ resources/views/map/partials/view-map.blade.php
@@ -2,19 +2,23 @@
   integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
   integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+<script src="./leaflet.edgebuffer.js"></script>
+
 
 <div id="map"
-  class="aspect-[2/1] w-full h-auto max-h-[350px] max-w-[700px] p-6 text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg m-auto">
+  class="m-auto aspect-[2/1] h-auto max-h-[350px] w-full max-w-[700px] overflow-hidden bg-white p-6 text-gray-900 shadow-sm sm:rounded-lg dark:bg-gray-800 dark:text-gray-100">
 </div>
 <script>
   var map = L.map('map').setView([43.5361003, 5.455119], 13);
-  var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 0,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
-  });
+  var Stadia_AlidadeSmoothDark = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+      minZoom: 0,
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: 'png'
+    });
   var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+    edgeBufferTiles: 1,
     minZoom: 0,
     maxZoom: 20,
     attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -32,6 +36,4 @@
     iconAnchor: [17, 17],
     popupAnchor: [0, 0]
   });
-
-
 </script>
